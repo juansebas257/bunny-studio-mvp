0dacb02aa6a75a9af3f1bfc606b7939d26e1144b
Sebastian Jimenez <juansebas257@gmail.com>
2020-10-30 19:47:21 -0500

    adding mockups and architecture diagram

--- users-service/app/server.js
+++ users-service/app/server.js
@@ -13,17 +13,41 @@ app.listen(port, () => {
     console.log(`user-services server running on port ${port}`);
 });
 
+
+// API REST for users
+// get all users
 app.get('/users', async function(req, res) {
     const users = await database.query(`SELECT * FROM users`, []);
     res.status(200).json(users);
 });
 
+// add user
 app.post('/users', async function(req, res) {
-    console.log(req)
-        // getting parameters
+    // getting parameters
     const name = req.body.name;
 
     // inserting new user
     const user = await database.query(`INSERT INTO users (name) VALUES (?)`, [name]);
+    res.status(200).json(user);
+});
+
+// update user
+app.put('/users/:id', async function(req, res) {
+    // getting parameters
+    const id = req.params.id;
+    const name = req.body.name;
+
+    // updating user
+    const user = await database.query(`UPDATE users SET name = ?, updated_at = NOW() WHERE id = ?`, [name, id]);
+    res.status(200).json(user);
+});
+
+// delete user
+app.delete('/users/:id', async function(req, res) {
+    // getting parameters
+    const id = req.params.id;
+
+    // deleting user
+    const user = await database.query(`DELETE FROM users WHERE id = ?`, [id]);
     res.status(200).json(user);
 });
